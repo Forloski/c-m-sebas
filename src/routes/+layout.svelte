b088bdf5895641d1e942db73dd9d41e35526ac9a
forloski <f.r.orloski@gmail.com>
2024-09-15 22:52:37 -0300

    added trailing slash to root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,9 @@
 	import { queryClient } from '../clients/query';
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from '$lib/components/ui/sonner';
+
+	export const prerender = true;
+	export const trailingSlash = 'always';
 </script>
 
 <ModeWatcher />
